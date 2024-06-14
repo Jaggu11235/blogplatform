c61dc3cb7b9b9da1e37cfb8417fde1616b377784
Renuguntla Jagadeesh <92438342+Jaggu11235@users.noreply.github.com>
2024-06-14 15:38:46 +0530

    Create blog_author.sql
    Author table creation SQL query

---
+++ blog_author.sql
@@ -0,0 +1,7 @@
+-- Authors Table
+CREATE TABLE authors (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL
+);
